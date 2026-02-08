e2534f21ead4cdda4d04caaa5d735abd3c1daadd
Narinebaghdasaryan <evrikaschool1@gmail.com>
2026-02-08 22:50:21 +0400

    avg_arr function added

--- utils/generic.py
+++ utils/generic.py
@@ -1,7 +1,11 @@
 import pandas as pd
+import numpy as np
 
 def read_file_with_pandas(file_name):
     csv_file_data=pd.read_csv(file_name)
     return csv_file_data
 
+
+def avg_arr(arr):
+    return arr.mean()
 
