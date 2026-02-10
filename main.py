4f0c3b73b340087978ce157b66576e1cd2e87203
Narinebaghdasaryan <evrikaschool1@gmail.com>
2026-02-10 18:56:06 +0400

    functions added

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-while True:
-    text = input("some text")
-    if text == "break":
-        break
+# while True:
+#     text = input("some text")
+#     if text == "break":
+#         break
+#
+
+import pandas as pd
+import numpy as np
+
+from utils.generic import read_file_with_pandas, max_min_arr, avg_arr
 
 
+df=pd.read_csv("tourist - tourist.csv")
+
+print(read_file_with_pandas("tourist - tourist.csv", "Days"))
+
+
+
+print(max_min_arr(df["Days"]))
+print(avg_arr(df["Days"]))
