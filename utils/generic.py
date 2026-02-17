8a06aad6516a5d7521b2fb8a32b5f7697e1deba7
Narinebaghdasaryan <evrikaschool1@gmail.com>
2026-02-17 20:38:16 +0400

    added missing files

--- utils/generic.py
+++ utils/generic.py
@@ -13,10 +13,28 @@ def read_file_with_pandas(file_name, column_name):
         return None
 
 
+def dataframe_info_with_pandas(file_name):
+    try:
+        csv_file_info=pd.read_csv(file_name )
+        return csv_file_info.info()
+    except FileNotFoundError:
+        print("No such file or directory: ->", f"{file_name}")
+
+
+def dataframe_columns(file_name):
+    try:
+        csv_file_info=pd.read_csv(file_name)
+        return list(csv_file_info.columns)
+    except FileNotFoundError:
+        print("No such file or directory: ->", f"{file_name}")
+
 
 
 def avg_arr(arr):
-    return arr.mean()
+    try:
+      return arr.mean()
+    except TypeError:
+        print("Cannot perform reduction 'mean' with string dtype")
 
 
 def max_min_arr(arr):
