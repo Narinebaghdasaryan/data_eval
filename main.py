4aa3ba634ec38d9c8126f6a79900bb6e3f0fbb37
Narinebaghdasaryan <evrikaschool1@gmail.com>
2026-02-20 19:46:27 +0400

    changes

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 import pandas as pd
 import numpy as np
 
-from utils.generic import read_file_with_pandas, max_min_arr, avg_arr, dataframe_info_with_pandas, dataframe_columns
+from utils.generic import read_file_with_pandas, max_min_arr, avg_arr, dataframe_info_with_pandas, read_column_with_pandas, dataframe_columns
 
 
 while True:
-    text = input("some file name")
+    text = input("Input file name ->  ")
     if text == "break":
         break
 
-    print(dataframe_columns(text))
+    read_dataframe = read_file_with_pandas(text)
 
-    dataframe_info=dataframe_info_with_pandas(text)
+    if read_dataframe is None:
+        continue
 
-    print(dataframe_info)
+    print(read_dataframe.head(5))
 
+    print(dataframe_columns(read_dataframe))
 
+    column_name = input("Input column name -> ")
 
-    col=(read_file_with_pandas(text, input("column name")))
-    if col is not None:
-        print(max_min_arr(col))
-        print(avg_arr(col))
+    column_data = read_column_with_pandas(read_dataframe, column_name)
+
+    print(dataframe_info_with_pandas(read_dataframe))
+
+    if column_data is not None:
+        print(max_min_arr(column_data))
+        print('mijin', avg_arr(column_data))
 
--- utils/generic.py
+++ utils/generic.py
@@ -1,44 +1,46 @@
 import pandas as pd
-import numpy as np
 
-def read_file_with_pandas(file_name, column_name):
+def read_file_with_pandas(file_name):
+
     try:
-        csv_file_data=pd.read_csv(file_name )
-        return csv_file_data[column_name]
-    except KeyError:
-        print("Key Error: Columns ->",list(csv_file_data.columns), f"Provided -> {column_name}")
-        return None
+        csv_file_data = pd.read_csv(file_name)
+        return csv_file_data
     except FileNotFoundError:
         print("File not found ->", f"{file_name} error")
         return None
 
 
-def dataframe_info_with_pandas(file_name):
+def read_column_with_pandas(csv_file_data, column_name):
+    if csv_file_data is None:
+        return None
     try:
-        csv_file_info=pd.read_csv(file_name )
-        return csv_file_info.info()
-    except FileNotFoundError:
-        print("No such file or directory: ->", f"{file_name}")
+        return  csv_file_data[column_name]
+    except KeyError:
+        print("Key Error: Columns ->", list(csv_file_data.columns), f"Provided -> {column_name}")
+        return None
+
+def dataframe_info_with_pandas(csv_file_data):
+    if csv_file_data is None:
+        return None
+    return  csv_file_data.info()
 
 
-def dataframe_columns(file_name):
+
+def dataframe_columns(csv_file_data):
+    if csv_file_data is None:
+        return None
+    return csv_file_data.columns
+
+
+def avg_arr(column_name):
     try:
-        csv_file_info=pd.read_csv(file_name)
-        return list(csv_file_info.columns)
-    except FileNotFoundError:
-        print("No such file or directory: ->", f"{file_name}")
-
-
-
-def avg_arr(arr):
-    try:
-      return arr.mean()
+      return column_name.mean()
     except TypeError:
         print("Cannot perform reduction 'mean' with string dtype")
 
 
-def max_min_arr(arr):
-    return arr.max(), arr.min()
+def max_min_arr(column_name):
+    return (f'max_value is in -> {column_name.max()} column  and min value is in ->  {column_name.min()} column')
 
 
 
